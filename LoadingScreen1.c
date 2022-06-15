eff290f4483851352e8f78323eaf1434b9e273a8
rishabh709 <97673849+rishabh709@users.noreply.github.com>
2022-06-15 20:13:26 +0530

    Add files via upload

---
+++ LoadingScreen1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main(){
+	int i;
+	char symbols[] = {'|','/','-','\\'};
+	while(1){
+		for(i=0; i<4; i++){
+			printf("\n\n\n\n\n\t\t\t LOADING: %c",symbols[i]);
+			system("cls");
+		}
+	}
+}
